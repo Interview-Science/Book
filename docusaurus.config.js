64590fb145c827b5a64c2aef37096aae365140d7
Interview-Science <hello@interview-science.com>
2021-08-19 19:27:02 +0800

    updata config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,13 +79,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/Interview-Science/Book/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/Interview-Science/Book/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
